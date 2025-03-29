4e1784e1aa9de583dffc28d85dba442e67002b8e
gustavowmarques <gustavowmarques@gmail.com>
2025-03-29 14:18:46 +0000

    custom error pages added for 404 and 500 errors with a test for 500 error

--- app.py
+++ app.py
@@ -266,9 +266,17 @@ def feedback():
 
     return render_template("feedback.html")
 
-# Email config
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
+@app.route("/trigger500")
+def trigger_error():
+    raise Exception("Deliberate error!")
 
 
 if __name__ =="__main__":
